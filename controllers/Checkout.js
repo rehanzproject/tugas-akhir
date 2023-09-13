90e6439c80513f12fecc1df7070d702ae5704cad
Rehan Maulana <rehanmaul111@gmail.com>
2023-09-13 18:37:58 +0700

    feat: add checkout function to checkout

--- controllers/Checkout.js
+++ controllers/Checkout.js
@@ -1,34 +1,108 @@
 import Checkout from "../model/CheckoutModel.js";
 
+export const getAllCheckout = async (req, res) => {
+  try {
+    const getCheckout = await Checkout.findAll();
+    if (!getCheckout.length)
+      return res.status(404).json({
+        code: 404,
+        status: "Not Found",
+        message: "Checkout Not Found",
+        success: false,
+      });
+
+    res.status(200).json({
+      code: 200,
+      status: "Created",
+      message: "Get All Checkout Successfully",
+      success: true,
+      data: getCheckout,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      code: 500,
+      status: "Internal Server Error",
+      message: "Internal Server Error",
+      errors: { error },
+    });
+  }
+};
+
 export const getCheckout = async (req, res) => {
   try {
-    const getCheckout = await Checkout.findAll({
+    // const getCheckout = await Checkout.findAll({
+    //   where: {
+    //     course_id: req.query.id,
+    //   },
+    // });
+    //  if(getCheckout.length) return res.status(400).json({code:400 ,status: "Bad Request",message:"Checkout has created", success: false});
+    const searchUser = await Checkout.findAll({
       where: {
+        user_id: req.userId,
         course_id: req.query.id,
+        verify: false,
       },
     });
-    if (!getCheckout)
-      return res.status(404).json({ msg: "Checkout Not Found!" });
-    await Checkout.create({
+    if (searchUser.length)
+      return res.status(400).json({
+        code: 400,
+        status: "Bad Request",
+        message: "User Already Checkout",
+        success: false,
+      });
+    const createCheckout = await Checkout.create({
       course_id: req.query.id,
       user_id: req.userId,
     });
-    res.status(200).json({ msg: "Get Checkout Success", data: getCheckout });
+    res.status(201).json({
+      code: 201,
+      status: "Created",
+      message: "User Checkout Successfully",
+      success: true,
+      data: createCheckout,
+    });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ msg: "Internal Server Error" });
+    res.status(500).json({
+      code: 500,
+      status: "Internal Server Error",
+      message: "Internal Server Error",
+      errors: { error },
+    });
   }
 };
 
 export const getCheckoutVerify = async (req, res) => {
   try {
-    const getCheckout = await Checkout.findAll({
+    const findUserAndCheckout = await Checkout.findOne({
       where: {
-        user_id: req.query.id,
+        id: req.query.id,
+        user_id: req.userId,
       },
     });
-    if (!getCheckout)
-      return res.status(404).json({ msg: "Checkout Not Found!" });
+    if (!findUserAndCheckout)
+      return res
+        .status(404)
+        .json({
+          code: 404,
+          status: "Not Found",
+          message: "Checkout Not Found",
+          success: false,
+        });
+    const addCheckoutVerify = await findUserAndCheckout.update({
+      payment_method: req.body.payment_method,
+      verify: true,
+    });
+    res
+      .status(201)
+      .json({
+        code: 201,
+        status: "Created",
+        message: "User Checkout Course Successfully",
+        success: true,
+        data: addCheckoutVerify,
+      });
   } catch (error) {
     console.log(error);
     res.status(500).json({ msg: "Internal Server Error" });
@@ -39,13 +113,23 @@ export const getCheckoutUser = async (req, res) => {
   try {
     const getCheckout = await Checkout.findOne({
       where: {
-        email: req.email
+        user_id: req.userId,
       },
     });
     if (!getCheckout)
-    return res.status(404).json({code:404 ,status: "Not Found",message:"User Not Found", success: false});
+      return res.status(404).json({
+        code: 404,
+        status: "Not Found",
+        message: "User Not Found",
+        success: false,
+      });
   } catch (error) {
     console.log(error);
-    res.status(500).json({code:500 ,status: "Internal Server Error",message:"Internal Server Error", errors: { error }});
+    res.status(500).json({
+      code: 500,
+      status: "Internal Server Error",
+      message: "Internal Server Error",
+      errors: { error },
+    });
   }
 };
