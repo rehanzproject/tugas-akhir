05efaaa0a90405e0be4be3643da2970ac2476e4f
Rehan Maulana <rehanmaul111@gmail.com>
2023-09-13 18:41:25 +0700

    feat: add more router

--- router/UserRouter.js
+++ router/UserRouter.js
@@ -1,12 +1,26 @@
 import express from "express";
 import { verifyToken } from "../middleware/VerifyToken.js";
-import { Login, Logout, Register, getUserByEmail, getUsers } from "../controllers/Users.js";
+import {
+  Login,
+  Logout,
+  Register,
+  getUserByEmail,
+  getUsers,
+} from "../controllers/Users.js";
 import { refreshToken } from "../controllers/RefreshToken.js";
 import { ForgotPassword } from "../controllers/ForgotPassword.js";
 import { ShowPDF } from "../controllers/PDFMaker.js";
-import { AddCourse, getCourse } from "../controllers/Course.js";
-import { getModules } from "../controllers/Modules.js";
-import { getCheckout, getCheckoutVerify } from "../controllers/Checkout.js";
+import {
+  AddCourse,
+  getCourse,
+  getCourseUserCheckout,
+} from "../controllers/Course.js";
+import { addModule, getModules } from "../controllers/Modules.js";
+import {
+  getAllCheckout,
+  getCheckout,
+  getCheckoutVerify,
+} from "../controllers/Checkout.js";
 import {
   SaveDocument,
   SavePicture,
@@ -14,6 +28,7 @@ import {
 } from "../controllers/UploadFile.js";
 
 const UserRouter = express.Router();
+// user
 UserRouter.get("/info", verifyToken, getUserByEmail);
 UserRouter.get("/ping", (req, res) => res.status(200).json({ msg: "PING!!" }));
 UserRouter.get("/all", getUsers);
@@ -22,14 +37,18 @@ UserRouter.post("/login", Login);
 UserRouter.get("/token", refreshToken);
 UserRouter.delete("/logout", Logout);
 UserRouter.post("/forgot-password", ForgotPassword);
-UserRouter.get("/getPDF",verifyToken, ShowPDF);
-// User course
+UserRouter.get("/getPDF", verifyToken, ShowPDF);
+// User course & modules
+UserRouter.get("/course/checkout", verifyToken, getCourseUserCheckout);
 UserRouter.post("/addcourse", AddCourse);
 UserRouter.get("/modules", getModules);
+UserRouter.post("/addmodule", verifyToken, addModule);
 UserRouter.get("/course", getCourse);
 // User Checkout
-UserRouter.post("/checkout", getCheckout);
-UserRouter.post("/checkout/verify", getCheckoutVerify);
+UserRouter.get("/checkout/history", verifyToken, getAllCheckout);
+UserRouter.post("/checkout", verifyToken, getCheckout);
+UserRouter.post("/checkout/verify", verifyToken, getCheckoutVerify);
+
 // Upload
 UserRouter.post(
   "/upload/picture",
