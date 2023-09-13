fb929d80e89376d38c0a298c04a2053748b8cf8b
Rehan Maulana <rehanmaul111@gmail.com>
2023-09-13 18:38:58 +0700

    feat: add Module to add a module

--- controllers/Modules.js
+++ controllers/Modules.js
@@ -1,4 +1,5 @@
 import Course from "../model/CourseModel.js";
+import Modules from "../model/ModulesModel.js";
 
 export const getModules = async (req, res) => {
   try {
@@ -9,6 +10,39 @@ export const getModules = async (req, res) => {
     });
     if (!getModule) return res.status(404).json({ msg: "Module Not Found!" });
     res.status(200).json({ msg: "Success Get Modules", data: getModule });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Internal Server Error!" });
+  }
+};
+
+export const addModule = async (req, res) => {
+  try {
+    const { name, desc } = req.body;
+    const getModule = await Course.findAll({
+      where: {
+        course_id: req.query.id,
+      },
+    });
+    if (!getModule.length)
+      return res.status(400).json({
+        code: 400,
+        status: "Bad Request",
+        message: "Module has existed",
+        success: false,
+      });
+    const createModule = await Modules.create({
+      course_id: req.query.id,
+      name: name,
+      desc: desc,
+    });
+    res.status(201).json({
+      code: 201,
+      status: "Created",
+      message: "Add Module Successfully",
+      success: true,
+      data: createModule,
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({ msg: "Internal Server Error!" });
